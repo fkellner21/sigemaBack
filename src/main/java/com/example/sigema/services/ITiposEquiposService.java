d2451ce71b7359599c171455f15849e436500b5b
joacoseo1 <joacoseo1@gmail.com>
2025-05-21 21:21:50 -0300

    Se creo controller con API de Tipos de Equipos

---
+++ src/main/java/com/example/sigema/services/ITiposEquiposService.java
@@ -0,0 +1,14 @@
+package com.example.sigema.services;
+import com.example.sigema.models.TipoEquipo;
+import java.util.List;
+import java.util.Optional;
+
+public interface ITiposEquiposService {
+    TipoEquipo Crear(TipoEquipo tipoEquipo) throws Exception;
+
+    TipoEquipo Editar(Long id, TipoEquipo tipoEquipo) throws Exception;
+
+    Optional<TipoEquipo> ObtenerPorId(Long id);
+
+    List<TipoEquipo> ObtenerTodos(boolean soloActivos);
+}

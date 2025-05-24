1d742dbf2ac8c5fe134fb7e30e2c55a356b4735e
joacoseo1 <joacoseo1@gmail.com>
2025-05-24 13:00:41 -0300

    Cambios github actions

---
+++ src/main/java/com/example/sigema/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.sigema.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HealthController {
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
+}
+

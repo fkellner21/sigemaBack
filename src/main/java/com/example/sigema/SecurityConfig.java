7391dd7c4d241c1af7c996000adfa9b9e2873b39
fkellner21 <kellner.francisco@gmail.com>
2025-08-01 11:51:26 -0300

    cambia uno de los setAllowedOrigins en del SecurityConfig sacando el *

--- src/main/java/com/example/sigema/SecurityConfig.java
+++ src/main/java/com/example/sigema/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "http://127.0.0.1:*",
+                "http://127.0.0.1:4200",
                 "https://jolly-smoke-0b3e9eb0f.6.azurestaticapps.net",
                 "https://sigemabe-d0gke3fdbnfza9et.canadacentral-01.azurewebsites.net",
                 "https://black-sky-08a0d770f.2.azurestaticapps.net"

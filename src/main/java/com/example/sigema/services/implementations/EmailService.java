55226d1a44599dc8d8b88ec6d82299ea57060795
cristianvelozzz <cr.velozz@gmail.com>
2025-08-02 15:57:06 -0300

    ajustes

--- src/main/java/com/example/sigema/services/implementations/EmailService.java
+++ src/main/java/com/example/sigema/services/implementations/EmailService.java
@@ -2,9 +2,11 @@ package com.example.sigema.services.implementations;
 
 import com.example.sigema.models.Equipo;
 import com.example.sigema.models.ModeloEquipo;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,19 +15,19 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void enviarAlertaMantenimiento(Equipo equipo, ModeloEquipo modelo, String asunto, boolean esCritico, String destinatario) {
-        String to = "mantenimiento@tusistema.com";
-        String body = "Equipo: " + equipo.getMatricula() +
-                "\nUnidad: " + (equipo.getUnidad() != null ? equipo.getUnidad().getNombre() : "N/A") +
-                "\nModelo: " + modelo.getModelo() +
-                "\nCantidad actual: " + equipo.getCantidadUnidadMedida() + " " + modelo.getUnidadMedida() +
-                "\nFrecuencia definida: " + modelo.getFrecuenciaUnidadMedida() + " " + modelo.getUnidadMedida();
+    public void enviarAlertaMantenimiento(Equipo equipo, ModeloEquipo modelo, String contenidoHtml, boolean esCritico, String destinatario) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(asunto);
-        message.setText(body);
+            helper.setTo(destinatario);
+            helper.setSubject(esCritico ? "🔴 ALERTA DE MANTENIMIENTO CRÍTICA" : "⚠️ Alerta preventiva de mantenimiento");
+            helper.setText(contenidoHtml, true);
 
-        mailSender.send(message);
+            mailSender.send(message);
+        } catch (Exception e) {
+            // Podés loguearlo o lanzar una excepción personalizada
+            System.err.println("Error al enviar el correo: " + e.getMessage());
+        }
     }
 }
